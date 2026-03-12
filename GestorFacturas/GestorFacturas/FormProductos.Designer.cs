aad3725144c94e2db36a9894856fd33825052c90
Jefagt <mygamesfg740@gmail.com>
2026-03-11 23:52:34 -0600

    Update TextBox

--- GestorFacturas/GestorFacturas/FormProductos.Designer.cs
+++ GestorFacturas/GestorFacturas/FormProductos.Designer.cs
@@ -35,15 +35,15 @@
             this.label3 = new System.Windows.Forms.Label();
             this.txtPrecio = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgbProductos = new System.Windows.Forms.DataGridView();
             this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgbProductos)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -107,12 +107,12 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Precio";
             // 
-            // textBox4
+            // txtStock
             // 
-            this.textBox4.Location = new System.Drawing.Point(140, 170);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(214, 20);
-            this.textBox4.TabIndex = 8;
+            this.txtStock.Location = new System.Drawing.Point(140, 170);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(214, 20);
+            this.txtStock.TabIndex = 8;
             // 
             // label5
             // 
@@ -124,13 +124,13 @@
             this.label5.TabIndex = 7;
             this.label5.Text = "Stock";
             // 
-            // dataGridView1
+            // dgbProductos
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(44, 237);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(705, 150);
-            this.dataGridView1.TabIndex = 9;
+            this.dgbProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgbProductos.Location = new System.Drawing.Point(44, 237);
+            this.dgbProductos.Name = "dgbProductos";
+            this.dgbProductos.Size = new System.Drawing.Size(705, 150);
+            this.dgbProductos.TabIndex = 9;
             // 
             // label6
             // 
@@ -192,8 +192,8 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dgbProductos);
+            this.Controls.Add(this.txtStock);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtPrecio);
             this.Controls.Add(this.label4);
@@ -204,7 +204,7 @@
             this.Controls.Add(this.label1);
             this.Name = "FormProductos";
             this.Text = "FormProductos";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgbProductos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,9 +219,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtPrecio;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtStock;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgbProductos;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
