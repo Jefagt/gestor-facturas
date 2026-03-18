fc52d8cb9a655bf7a127189142d5aacec6a5df70
Jefagt <mygamesfg740@gmail.com>
2026-03-18 13:55:41 -0600

    Update FormFacturas.Designer.cs

--- GestorFacturas/GestorFacturas/FormFacturas.Designer.cs
+++ GestorFacturas/GestorFacturas/FormFacturas.Designer.cs
@@ -45,6 +45,7 @@
             this.btnEliminarFactura = new System.Windows.Forms.Button();
             this.btnListarFacturas = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFacturas)).BeginInit();
             this.SuspendLayout();
             // 
@@ -213,11 +214,22 @@
             this.label7.TabIndex = 16;
             this.label7.Text = "Lista de Facturas";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(588, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Agregar Detalle";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormFacturas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.btnListarFacturas);
             this.Controls.Add(this.btnEliminarFactura);
@@ -262,5 +274,6 @@
         private System.Windows.Forms.Button btnEliminarFactura;
         private System.Windows.Forms.Button btnListarFacturas;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
     }
 }
