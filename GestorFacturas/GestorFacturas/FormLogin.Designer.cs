1521c88212d951dada3de6a2f31933a5f89a57e3
Jefagt <mygamesfg740@gmail.com>
2026-03-21 22:58:22 -0600

    Update FormLogin.Designer.cs

--- GestorFacturas/GestorFacturas/FormLogin.Designer.cs
+++ GestorFacturas/GestorFacturas/FormLogin.Designer.cs
@@ -35,6 +35,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label1.Location = new System.Drawing.Point(243, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(312, 29);
@@ -46,6 +47,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(806, 375);
             this.Controls.Add(this.label1);
             this.Name = "FormLogin";
