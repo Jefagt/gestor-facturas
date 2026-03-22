acab10abe9f3e6349beb90e463bfb546d52c16f3
Jefagt <mygamesfg740@gmail.com>
2026-03-21 22:27:05 -0600

    Update Program.cs

--- GestorFacturas/GestorFacturas/Program.cs
+++ GestorFacturas/GestorFacturas/Program.cs
@@ -16,7 +16,7 @@ namespace GestorFacturas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormLogin());
         }
     }
 }
