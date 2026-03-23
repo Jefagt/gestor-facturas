8c4b7be67995fd84ab50e22d62c9fd8306a3d73a
Jefagt <mygamesfg740@gmail.com>
2026-03-22 21:59:41 -0600

    Update FormLogin.Designer.cs

--- GestorFacturas/GestorFacturas/FormLogin.Designer.cs
+++ GestorFacturas/GestorFacturas/FormLogin.Designer.cs
@@ -91,7 +91,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(149, 109);
+            this.pictureBox1.Location = new System.Drawing.Point(143, 109);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 88);
             this.pictureBox1.TabIndex = 1;
@@ -102,7 +102,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.label3.Location = new System.Drawing.Point(135, 209);
+            this.label3.Location = new System.Drawing.Point(129, 209);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(127, 26);
             this.label3.TabIndex = 2;
