dbaf516e207645955b97a186f6753b4df768ea2c
Jefagt <mygamesfg740@gmail.com>
2026-03-29 12:38:41 -0600

    Update FormPrincipal.Designer.cs

--- GestorFacturas/GestorFacturas/FormPrincipal.Designer.cs
+++ GestorFacturas/GestorFacturas/FormPrincipal.Designer.cs
@@ -28,15 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
             this.SuspendLayout();
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormPrincipal";
-            this.Text = "FormPrincipal";
+            this.Text = "Sistema de getión de negocios - Dashboard";
             this.Load += new System.EventHandler(this.FormPrincipal_Load);
             this.ResumeLayout(false);
 
