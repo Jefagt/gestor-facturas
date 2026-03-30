dedc7416cd5e9a6af5932172f04866390bf1ea68
Jefagt <mygamesfg740@gmail.com>
2026-03-30 12:46:55 -0600

    Update FormPrincipal.Designer.cs

--- GestorFacturas/GestorFacturas/FormPrincipal.Designer.cs
+++ GestorFacturas/GestorFacturas/FormPrincipal.Designer.cs
@@ -33,16 +33,19 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(834, 31);
+            this.panel1.Size = new System.Drawing.Size(834, 28);
             this.panel1.TabIndex = 0;
             // 
             // panel2
@@ -50,7 +53,7 @@
             this.panel2.BackColor = System.Drawing.Color.Silver;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 838);
+            this.panel2.Location = new System.Drawing.Point(0, 726);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(834, 23);
             this.panel2.TabIndex = 1;
@@ -60,35 +63,50 @@
             this.panel3.BackColor = System.Drawing.Color.Silver;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel3.Location = new System.Drawing.Point(0, 31);
+            this.panel3.Location = new System.Drawing.Point(0, 28);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 807);
+            this.panel3.Size = new System.Drawing.Size(200, 698);
             this.panel3.TabIndex = 2;
             // 
             // panel4
             // 
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(200, 31);
+            this.panel4.Location = new System.Drawing.Point(200, 28);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(634, 807);
+            this.panel4.Size = new System.Drawing.Size(634, 698);
             this.panel4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(25, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema de Gestión de Empresas v1.0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(834, 861);
+            this.ClientSize = new System.Drawing.Size(834, 749);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(850, 900);
+            this.MinimumSize = new System.Drawing.Size(850, 766);
             this.Name = "FormPrincipal";
-            this.Text = "Sistema de getión de negocios - Dashboard";
+            this.Text = "s";
             this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -99,5 +117,6 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
     }
 }
