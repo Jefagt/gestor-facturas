aff530f3e0a66b8376efd93bea26de0fad958699
Jefagt <mygamesfg740@gmail.com>
2026-04-05 22:44:19 -0600

    Update FormDashboard.Designer.cs

--- GestorFacturas/GestorFacturas/FormDashboard.Designer.cs
+++ GestorFacturas/GestorFacturas/FormDashboard.Designer.cs
@@ -120,11 +120,13 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 13);
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label3.Location = new System.Drawing.Point(31, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(146, 19);
             this.label3.TabIndex = 0;
-            this.label3.Text = "label3";
+            this.label3.Text = "TOTAL CLIENTES";
             // 
             // tableLayoutPanel2
             // 
